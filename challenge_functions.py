aa15ec9d5386698945a62b7efce16224ee1a5b9f
ambilikn <ambilikn@gmail.com>
2021-05-19 11:27:49 +0530

    Update challenge_functions.py
    Changed to function and printing in reverse order

--- challenge_functions.py
+++ challenge_functions.py
@@ -1,12 +1,22 @@
+#To get the count of each character in a word
+import operator
+
+def count_characters(word):
+    dict = {}
+
+    print(dict)
+
+    for i in word:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    print("The count of characters in the word "+ word+" is "+ str(dict))
+    # Sort Dictionary by value using itemgetter
+    sorted_dict = sorted( dict.items(), key=operator.itemgetter(1), reverse=True)
+    print('Sorted Dictionary: ')
+    print(sorted_dict)
+
+
 word = input("Enter a word")
-Dict = {}
-
-print(Dict)
-
-for i in word:
-    if i in Dict:
-        Dict[i] += 1
-    else:
-        Dict[i] = 1
-
-print("The count of characters in the word "+ word+"is "+ str(Dict))
+count_characters(word)
